5cb5d1211ba6287aaa9cd75234d452a7a9ff370f
Ivaylo Dyakov <ipd3995@gmail.com>
2020-08-14 11:56:25 +0200

    Finished routing and controllers

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(cocktail_id: params[:cocktail_id], ingredient_id: params[:dose][:ingredient_id], description: params[:dose][:description])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+end
